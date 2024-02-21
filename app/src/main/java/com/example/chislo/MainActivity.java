b3526e8bbce0e48d1114a7cab04c4a4e2f3ebbda
LolKIa <remeyztoor@gmail.com>
2024-02-21 20:09:32 +0900

    21feb

---
+++ app/src/main/java/com/example/chislo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.chislo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvInfo;
+    EditText etInput;
+    Button bControl;
+    int guess;
+    boolean gameFinished;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView)findViewById(R.id.textView1);
+        etInput = (EditText)findViewById(R.id.editText1);
+        bControl = (Button)findViewById(R.id.button1);
+
+        guess = (int)(Math.random()*100);
+        gameFinished = false;
+
+    }
+
+    public void OnClick(View v) {
+        if (!gameFinished) {
+            if (!etInput.getText().toString().isEmpty()) {
+                int inp = Integer.parseInt(etInput.getText().toString());
+                if (inp > guess)
+                    tvInfo.setText(getResources().getString(R.string.ahead));
+                if (inp < guess)
+                    tvInfo.setText(getResources().getString(R.string.behind));
+                if (inp == guess) {
+                    tvInfo.setText(getResources().getString(R.string.hit));
+                    bControl.setText(getResources().getString(R.string.play_more));
+                    gameFinished = true;
+                }
+            } else {
+                Toast.makeText(this, "Пожалуйста, введите значение", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            guess = (int)(Math.random()*100);
+            bControl.setText(getResources().getString(R.string.input_value));
+            tvInfo.setText(getResources().getString(R.string.try_to_guess));
+            gameFinished = false;
+        }
+    }
+
+
+
+
+}
